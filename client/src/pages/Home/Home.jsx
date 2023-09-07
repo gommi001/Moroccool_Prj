e8b3bee515459d58d058efbd4facc1fd82baa8fc
lilamme21 <edderrazamine15@gmail.com>
2023-09-07 18:57:21 +0100

    code cleaning

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,13 +4,10 @@ import Hero from "../../components/hero/Hero"
 import Destinations from '../../components/destinations/Destinations'
 import DesHero from '../../components/deshero/DesHero'
 import Luxury from "../../components/luxury/Luxury"
-import Driver from "../../components/driver/Driver"
 import Selects from "../../components/selects/Selects"
 import Find from "../../components/homeswipertypes/Find"
 import Foot from "../../components/foot/Footer"
 import FooterBanner from "../../components/footbanner/FooterBanner"
-import Subscribe from "../../components/contact/Subscribe"
-import Gallery from "../../components/lightbox/Gallery"
 import { Scrollbars } from 'react-custom-scrollbars'
 import Description from "../../components/DescMoroccool/Description"
 import ScrollToTop from "../../components/ScrollToTop"
