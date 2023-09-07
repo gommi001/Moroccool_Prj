878893e46e2810ec03b4fee0ae602521489c5cf1
lilamme21 <edderrazamine15@gmail.com>
2023-09-07 18:52:01 +0100

    Home updates

--- client/src/components/luxury/Luxury.jsx
+++ client/src/components/luxury/Luxury.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
 import styles from './Luxury.module.css'
 import {Link} from 'react-router-dom'
-
+import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
 
 const Luxury = () => {
   return (
     <div className={styles.luxury}>
         <div className={styles.heading}>
             <h1>Find Your Destination</h1>
-        </div>
+        </div>     
         
-      <div className={styles.container}>
-      
+        <Link to='/destination'>
+            <h6 className={styles.viewMoreButton}>Find More <ArrowCircleRightRoundedIcon fontSize='medium' /> </h6>
+        </Link>
+
+      <div className={styles.container}>   
+        <a href='/destination/meknes' style={{color:'black'}}>
         <div className={styles.card}>
             <img src="https://images.unsplash.com/photo-1612171579482-daee4955f5ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
             <div className={styles.content}>
                 <h3>Meknes</h3>
-                <p>Meknes is one of the four Imperial cities of Morocco...</p>
-                <button>Visit Meknes</button>
+                <p>Meknes is one of the four Imperial cities </p>       
             </div>
-        </div>        
+        </div> 
+        </a>       
       
         <div className={styles.card}>
             <img src="https://images.unsplash.com/photo-1585336897192-540964ec47f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bWFycmFrZWNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
             <div className={styles.content}>
                 <h3>Marrakech</h3>
                 <p>Marrakech is one of the four Imperial cities of Morocco...</p>
-                <button>Visit Meknes</button>
+                
             </div>
         </div>        
       
@@ -35,7 +39,7 @@ const Luxury = () => {
             <div className={styles.content}>
                 <h3>Essaouira</h3>
                 <p>Meknes is one of the four Imperial cities of Morocco...</p>
-                <button>Visit Meknes</button>
+                
             </div>
         </div>
       
@@ -44,13 +48,11 @@ const Luxury = () => {
             <div className={styles.content}>
                 <h3>Casablanca</h3>
                 <p>Meknes is one of the four Imperial cities of Morocco...</p>
-                <button>Visit Meknes</button>
+                
             </div>
         </div>     
         
         </div>
-        
-        <Link to='/destination'><button style={{}}>More Destinations...</button></Link>
         
         
     </div>
--- client/src/components/selects/Selects.js
+++ client/src/components/selects/Selects.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import './SelectsStyles.css'
 import { Link } from 'react-router-dom'
-
-
-import SelectsImg from '../SelectsImg/SelectsImg'
+import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
 
 
 function Selects() {
     return (
         <div name='views' className='selects'>
             <h1>Exclusive Events</h1>
-            <h5>Discover Traditional and New Events</h5>
+            <h6>Discover Traditional and New Events</h6>
+
+            <Link to='/events'>
+                <h6 className='viewMoreButton'>View More ... <ArrowCircleRightRoundedIcon fontSize='medium' /> </h6>
+            </Link>
             
             <div className='container'>
-                <SelectsImg bgImg='https://gcdn.imgix.net/events/les-princesses-du-chaabi-de-retour-a-la-fol-1.jpeg?w=450&h=450&fit=crop&crop=entropy&auto=format,compress&q=80' text='Bidawa Maroc' />
-                <SelectsImg bgImg='https://gcdn.imgix.net/events/concert-electro-samifati-1.jpg?w=450&h=450&fit=crop&crop=entropy&auto=format,compress&q=80' text='SamiFati AltF4' />
-                <SelectsImg bgImg='https://gcdn.imgix.net/events/2rDJ1XBTkTXguCK6UGCvAe8MOKbvnLdl7l5B1dOa.jpeg?w=450&h=450&fit=crop&crop=entropy&auto=format,compress&q=80' text='Mamoun AlTab' />
-                <SelectsImg bgImg='https://gcdn.imgix.net/events/4sgyAdYKXVeMQNQDGgDSRuB8utV22goj6gAmtdCy.jpeg' text='Gnawa Event' />
-                <SelectsImg bgImg='https://gcdn.imgix.net/events/4sgyAdYKXVeMQNQDGgDSRuB8utV22goj6gAmtdCy.jpeg' text='Gnawa Event' />
-                <SelectsImg bgImg='https://gcdn.imgix.net/events/hommage-aux-divas-marocaines.jpg?w=450&h=450&fit=crop&crop=entropy&auto=format,compress&q=80' text='SamiFati AltF4' />
-            </div>
-            <Link to='/events'> <button className='more_des'>View All...</button> </Link>
+                <div className='selects-location'>
+                    <img src='https://gcdn.imgix.net/events/didib-en-concert-1.jpeg?w=450&h=450&fit=crop&crop=entropy&auto=format,compress&q=80' alt='/' />
+                </div>
+                <div className='selects-location'>
+                    <img src='https://gcdn.imgix.net/events/pinocchio-a-casablanca-seance-15h00.jpeg?w=450&h=450&fit=crop&crop=entropy&auto=format,compress&q=80' alt='/' />
+                </div>
+                <div className='selects-location'>
+                    <img src='https://gcdn.imgix.net/events/hassan-el-fad-who-is-kabour-a-casablanca-6.jpg?w=450&h=450&fit=crop&crop=entropy&auto=format,compress&q=80' alt='/' />
+                </div>
+            </div>           
 
         </div>
     )
