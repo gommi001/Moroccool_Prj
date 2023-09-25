f18bc6ef0dca4f4ffa514492c3b7763fbeecfdad
ozcodev <43305158+ozcodev@users.noreply.github.com>
2023-09-25 23:00:21 +0100

    FooterCopyRights

--- client/src/components/foot/Footer.jsx
+++ client/src/components/foot/Footer.jsx
@@ -1,38 +1,36 @@
-import React, { useEffect, useState } from "react";
-import "./Footer.scss";
+import React, { useEffect, useState } from 'react';
+import './Footer.scss';
 import PaymentIcon from '@mui/icons-material/Payment';
 import PriceChangeIcon from '@mui/icons-material/PriceChange';
-import axios from 'axios'
-import logo from '../../assets/moroccol_logo.png'
+import axios from 'axios';
+import logo from '../../assets/moroccol_logo.png';
 import LocationOnRoundedIcon from '@mui/icons-material/LocationOnRounded';
 const Footer = () => {
+  // const ipLo = document.getElementById('id')
+  // const countryLo = document.getElementById('country')
+  // const cityLo = document.getElementById('city')
 
-      // const ipLo = document.getElementById('id')
-      // const countryLo = document.getElementById('country')
-      // const cityLo = document.getElementById('city')
+  //   fetch('http://ip-api.com/json/?fields=61439')
+  //       .then(res => res.json())
+  //       .then(res => console.log(res));
 
-        
-      //   fetch('http://ip-api.com/json/?fields=61439')
-      //       .then(res => res.json())
-      //       .then(res => console.log(res));
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [code, setCode] = useState('');
 
-      const [city, setCity] = useState('');
-      const [country, setCountry] = useState('');
-      const [code, setCode] = useState('');
-  
-        useEffect(() => {
-          // Fetch user's city using ip-api.com API
-          axios.get('http://ip-api.com/json')
-            .then(response => {
-              setCity(response.data.city);
-              setCountry(response.data.country);
-              setCode(response.data.countryCode);
-            })
-            .catch(error => {
-              console.error('Error fetching city:', error);
-            });
-        }, []);
-     
+  useEffect(() => {
+    // Fetch user's city using ip-api.com API
+    axios
+      .get('http://ip-api.com/json')
+      .then((response) => {
+        setCity(response.data.city);
+        setCountry(response.data.country);
+        setCode(response.data.countryCode);
+      })
+      .catch((error) => {
+        console.error('Error fetching city:', error);
+      });
+  }, []);
 
   return (
     <div className="foot">
@@ -54,31 +52,27 @@ const Footer = () => {
           <span>Cookies</span>
         </div>
 
-         <div className="item">
+        <div className="item">
           <h1>About</h1>
           <span>Male </span>
           <span>Pages</span>
           <span>Stores</span>
           <span>Compare</span>
           <span>Cookies</span>
-        </div>     
-        
-        
+        </div>
       </div>
       <div className="bottom">
         <div className="left">
-          <img src={logo} alt="" className="logo"/> 
+          <img src={logo} alt="" className="logo" />
         </div>
-        
         <div className="right">
-        {city ? (
-        <p><LocationOnRoundedIcon/> {country} , {city}</p>
-      ) : (
-        <p>Loading city...</p>
-      )}      
+          © 2023 Moroccool. All Rights Reserved. Designed with ❤️ by&nbsp;
+          <a href="https://www.gb-digital.net"> GB-Digital</a>
+        </div>
+        <div className="lll">
+          <div>a b c</div>
         </div>
       </div>
-      
     </div>
   );
 };
